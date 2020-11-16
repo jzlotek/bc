e598e9ec0564f3c9c7982be6930d82a0d930ed4f
John Zlotek <jzlotek@gmail.com>
2020-11-15 19:51:52 -0500

    init files

---
+++ main.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"log"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getHome(ctx *gin.Context) {
+	ctx.JSON(http.StatusOK, map[string]interface{}{"data": "Hello world!"})
+}
+
+func main() {
+	var addr *string
+	addr = flag.String("bind", ":8000", "address to bind to")
+	flag.Parse()
+
+	router := gin.Default()
+	router.GET("/", getHome)
+
+	srv := &http.Server{
+		Addr:    *addr,
+		Handler: router,
+	}
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	quit := make(chan os.Signal)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
+}
