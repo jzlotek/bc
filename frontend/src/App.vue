74721ead5a58ce57855e284bdd7c15fe8219a7a6
John Zlotek <jzlotek@gmail.com>
2020-11-23 16:05:04 -0500

    backend frontend

---
+++ frontend/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png" />
+  <Card msg="Welcome to Your Vue.js App" />
+</template>
+
+<script>
+import Card from "./components/Card.vue";
+
+export default {
+  name: "App",
+  components: {
+    Card
+  }
+};
+</script>
+
+<style lang="scss">
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
